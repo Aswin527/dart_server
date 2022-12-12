d8c9abaf3ea772a265deb95f074532ad01da2b0f
Aswin Ramesh <aswinramesh527@gmail.com>
2022-12-12 23:40:54 +0530

    Dart Server Application

---
+++ bin/config.dart
@@ -0,0 +1,11 @@
+class FirebaseConfiguration {
+  static final firebaseConfig = {
+    'apiKey': "AIzaSyA0LF_VojflqDY_Ul38Bc5a6Zv7vk56qvU",
+    'authDomain': "test-52243.firebaseapp.com",
+    'projectId': "test-52243",
+    'storageBucket': "test-52243.appspot.com",
+    'messagingSenderId': "1092666346766",
+    'appId': "1:1092666346766:web:7244f6e051119863269b5f",
+    'measurementId': "G-5E12X5F027"
+  };
+}
